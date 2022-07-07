00023caa00a29cff785a4e909d65a9720f7fd68f
Gabriel Alencar <gfalencar02@gmail.com>
2022-07-07 11:52:55 -0300

    Implements DepartamentoDaoImpl and Departamento DAO

---
+++ src/main/java/com/gabrielfalencar/boot/demomvc/dao/DepartamentoDaoImpl.java
@@ -0,0 +1,9 @@
+package com.gabrielfalencar.boot.demomvc.dao;
+
+import com.gabrielfalencar.boot.demomvc.domain.Departamento;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class DepartamentoDaoImpl extends AbstractDao<Departamento, Long> implements DepartamentoDao{
+
+}
