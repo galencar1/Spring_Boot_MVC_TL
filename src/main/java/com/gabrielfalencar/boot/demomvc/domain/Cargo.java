ec090480271c61f4bb4f92e19c919b5c5ace73f6
Gabriel Alencar <gfalencar02@gmail.com>
2022-07-07 10:01:26 -0300

    Update cargo entity

--- src/main/java/com/gabrielfalencar/boot/demomvc/domain/Cargo.java
+++ src/main/java/com/gabrielfalencar/boot/demomvc/domain/Cargo.java
@@ -1,6 +1,7 @@
 package com.gabrielfalencar.boot.demomvc.domain;
 
 import javax.persistence.*;
+import java.util.List;
 
 @Entity
 @Table(name = "CARGOS")
@@ -12,6 +13,10 @@ public class Cargo extends AbstractEntity<Long>{
     @ManyToOne //Muitos cargos para um departamento
     @JoinColumn(name = "id_departamento_fk") // Faz referência ao nome da FK na tabela cargos
     private Departamento departamento; // Faz referência a chave estrangeira (FK) da tabela Departamentos
+
+    //Mapeando os funcionários
+    @OneToMany(mappedBy = "cargo") // A Tabela que tem a FK é o  lado forte. No mappedBy declaramos o lado fraco
+    private List<Funcionario> funcionarios;
 
 //    Getter e Setter para nome e departamento
     public String getNome() {
@@ -28,5 +33,13 @@ public class Cargo extends AbstractEntity<Long>{
 
     public void setDepartamento(Departamento departamento) {
         this.departamento = departamento;
+    }
+
+    public List<Funcionario> getFuncionarios() {
+        return funcionarios;
+    }
+
+    public void setFuncionarios(List<Funcionario> funcionarios) {
+        this.funcionarios = funcionarios;
     }
 }
