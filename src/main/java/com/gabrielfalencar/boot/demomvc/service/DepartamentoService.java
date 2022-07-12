b5ed61ef13cf206148fded97662da0bb0894ce37
Gabriel Alencar <gfalencar02@gmail.com>
2022-07-12 13:11:06 -0300

    ending implements service

--- src/main/java/com/gabrielfalencar/boot/demomvc/service/DepartamentoService.java
+++ src/main/java/com/gabrielfalencar/boot/demomvc/service/DepartamentoService.java
@@ -1,4 +1,18 @@
 package com.gabrielfalencar.boot.demomvc.service;
 
+import com.gabrielfalencar.boot.demomvc.domain.Departamento;
+
+import java.util.List;
+
 public interface DepartamentoService {
+    void salvar (Departamento departamento);
+
+    void editar (Departamento departamento);
+
+    void excluir (Long id);
+
+    Departamento buscaPorId(Long id);
+
+    List<Departamento> buscarTodos();
+
 }
