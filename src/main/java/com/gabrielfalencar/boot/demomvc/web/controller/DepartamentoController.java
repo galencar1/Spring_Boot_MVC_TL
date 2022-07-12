5f390048780e14fbb324ae001a47b838401daf1d
Gabriel Alencar <gfalencar02@gmail.com>
2022-07-12 16:53:52 -0300

    Insert departaments

--- src/main/java/com/gabrielfalencar/boot/demomvc/web/controller/DepartamentoController.java
+++ src/main/java/com/gabrielfalencar/boot/demomvc/web/controller/DepartamentoController.java
@@ -1,21 +1,34 @@
 package com.gabrielfalencar.boot.demomvc.web.controller;
 
+import com.gabrielfalencar.boot.demomvc.domain.Departamento;
+import com.gabrielfalencar.boot.demomvc.service.DepartamentoService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/departamentos") // Camimho na HOME.html
 public class DepartamentoController {
 
+    @Autowired
+    private DepartamentoService service;
+
     @GetMapping("/cadastrar") // caminho na HOME.html
-    public String cadastrar(){
+    public String cadastrar(Departamento departamento){
         return "/departamento/cadastro"; // caminho da pasta dentro do resources
     }
 
     @GetMapping("/listar")
     public String listar(){
         return "departamento/lista";
+    }
+
+    @PostMapping("/salvar")
+    public String salvar(Departamento departamento){
+        service.salvar(departamento);
+        return "redirect:/departamentos/cadastrar";
     }
 }
 
