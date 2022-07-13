075b0a696e98f66cef249a2a6bb20b320e311aca
Gabriel Alencar <gfalencar02@gmail.com>
2022-07-13 09:43:45 -0300

    listing positions

--- src/main/java/com/gabrielfalencar/boot/demomvc/web/controller/CargoController.java
+++ src/main/java/com/gabrielfalencar/boot/demomvc/web/controller/CargoController.java
@@ -6,6 +6,7 @@ import com.gabrielfalencar.boot.demomvc.service.CargoService;
 import com.gabrielfalencar.boot.demomvc.service.DepartamentoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -30,7 +31,8 @@ public class CargoController {
     }
 
     @GetMapping("/listar")
-    public String listar(){
+    public String listar(ModelMap model){
+        model.addAttribute("cargos", service.buscarTodos());
         return "cargo/lista";
     }
 
